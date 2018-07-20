f10d0ae50ce9bf6fdf4f4e009274bc48b2bcf467
Abhith Rajan <dearabhith@gmail.com>
2018-07-20 11:48:13 +0530

    login page initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:piggy_flutter/ui/page/login/login_page.dart';
 
 void main() => runApp(new MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Piggy',
       theme: new ThemeData(
         // This is the theme of your application.
         //
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new LoginPage(),
+//      home: new MyHomePage(title: 'Dashboard'),
     );
   }
 }
---
+++ lib/utils/uidata.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class UIData {
+  //routes
+  static const String homeRoute = "/home";
+  static const String profileOneRoute = "/View Profile";
+  static const String profileTwoRoute = "/Profile 2";
+  static const String notFoundRoute = "/No Search Result";
+  static const String timelineOneRoute = "/Feed";
+  static const String timelineTwoRoute = "/Tweets";
+  static const String settingsOneRoute = "/Device Settings";
+  static const String shoppingOneRoute = "/Shopping List";
+  static const String shoppingTwoRoute = "/Shopping Details";
+  static const String shoppingThreeRoute = "/Product Details";
+  static const String paymentOneRoute = "/Credit Card";
+  static const String paymentTwoRoute = "/Payment Success";
+  static const String loginOneRoute = "/Login With OTP";
+  static const String loginTwoRoute = "/Login 2";
+  static const String dashboardOneRoute = "/Dashboard 1";
+  static const String dashboardTwoRoute = "/Dashboard 2";
+
+  //strings
+  static const String appName = "Flutter UIKit";
+
+  //fonts
+  static const String quickFont = "Quicksand";
+  static const String ralewayFont = "Raleway";
+  static const String quickBoldFont = "Quicksand_Bold.otf";
+  static const String quickNormalFont = "Quicksand_Book.otf";
+  static const String quickLightFont = "Quicksand_Light.otf";
+
+  //images
+  static const String imageDir = "assets/images";
+  static const String pkImage = "$imageDir/pk.jpg";
+  static const String profileImage = "$imageDir/profile.jpg";
+  static const String blankImage = "$imageDir/blank.jpg";
+  static const String dashboardImage = "$imageDir/dashboard.jpg";
+  static const String loginImage = "$imageDir/login.jpg";
+  static const String paymentImage = "$imageDir/payment.jpg";
+  static const String settingsImage = "$imageDir/setting.jpeg";
+  static const String shoppingImage = "$imageDir/shopping.jpeg";
+  static const String timelineImage = "$imageDir/timeline.jpeg";
+  static const String verifyImage = "$imageDir/verification.jpg";
+
+  //login
+  static const String enter_code_label = "Phone Number";
+  static const String enter_code_hint = "10 Digit Phone Number";
+  static const String enter_otp_label = "OTP";
+  static const String enter_otp_hint = "4 Digit OTP";
+  static const String get_otp = "Get OTP";
+  static const String resend_otp = "Resend OTP";
+  static const String login = "Login";
+  static const String enter_valid_number = "Enter 10 digit phone number";
+  static const String enter_valid_otp = "Enter 4 digit otp";
+
+  //gneric
+  static const String error = "Error";
+  static const String success = "Success";
+  static const String ok = "OK";
+  static const String forgot_password = "Forgot Password?";
+  static const String something_went_wrong = "Something went wrong";
+  static const String coming_soon = "Coming Soon";
+
+  static const MaterialColor ui_kit_color = Colors.grey;
+
+//colors
+  static List<Color> kitGradients = [
+    // new Color.fromRGBO(103, 218, 255, 1.0),
+    // new Color.fromRGBO(3, 169, 244, 1.0),
+    // new Color.fromRGBO(0, 122, 193, 1.0),
+    Colors.blueGrey.shade800,
+    Colors.black87,
+  ];
+  static List<Color> kitGradients2 = [
+    Color(0xffb7ac50),
+    Colors.orange.shade900
+  ];
+
+  //randomcolor
+  static final Random _random = new Random();
+
+  /// Returns a random color.
+  static Color next() {
+    return new Color(0xFF000000 + _random.nextInt(0x00FFFFFF));
+  }
+}

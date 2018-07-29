bea16a9b82f76c9124882bba9673433fa8663f5e
Abhith Rajan <dearabhith@gmail.com>
2018-07-30 00:52:51 +0530

    chore (cleaned)

--- lib/utils/uidata.dart
+++ lib/utils/uidata.dart
@@ -1,6 +1,3 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class UIData {
@@ -14,38 +11,14 @@ class UIData {
   static const String appVersion = "0.0.1";
   static const String authToken = "authToken";
 
-  //fonts
-  static const String quickFont = "Quicksand";
-  static const String ralewayFont = "Raleway";
-  static const String quickBoldFont = "Quicksand_Bold.otf";
-  static const String quickNormalFont = "Quicksand_Book.otf";
-  static const String quickLightFont = "Quicksand_Light.otf";
-
   //images
   static const String imageDir = "assets/images";
-  static const String pkImage = "$imageDir/pk.jpg";
-  static const String profileImage = "$imageDir/profile.jpg";
-  static const String blankImage = "$imageDir/blank.jpg";
-  static const String dashboardImage = "$imageDir/dashboard.jpg";
   static const String loginImage = "$imageDir/login.jpg";
-  static const String paymentImage = "$imageDir/payment.jpg";
-  static const String settingsImage = "$imageDir/setting.jpeg";
-  static const String shoppingImage = "$imageDir/shopping.jpeg";
-  static const String timelineImage = "$imageDir/timeline.jpeg";
-  static const String verifyImage = "$imageDir/verification.jpg";
 
   //login
-  static const String enter_code_label = "Phone Number";
-  static const String enter_code_hint = "10 Digit Phone Number";
-  static const String enter_otp_label = "OTP";
-  static const String enter_otp_hint = "4 Digit OTP";
-  static const String get_otp = "Get OTP";
-  static const String resend_otp = "Resend OTP";
   static const String login = "Login";
-  static const String enter_valid_number = "Enter 10 digit phone number";
-  static const String enter_valid_otp = "Enter 4 digit otp";
 
-  //gneric
+  //generic
   static const String error = "Error";
   static const String success = "Success";
   static const String ok = "OK";
@@ -54,25 +27,4 @@ class UIData {
   static const String coming_soon = "Coming Soon";
 
   static const MaterialColor ui_kit_color = Colors.grey;
-
-//colors
-  static List<Color> kitGradients = [
-    // new Color.fromRGBO(103, 218, 255, 1.0),
-    // new Color.fromRGBO(3, 169, 244, 1.0),
-    // new Color.fromRGBO(0, 122, 193, 1.0),
-    Colors.blueGrey.shade800,
-    Colors.black87,
-  ];
-  static List<Color> kitGradients2 = [
-    Color(0xffb7ac50),
-    Colors.orange.shade900
-  ];
-
-  //randomcolor
-  static final Random _random = new Random();
-
-  /// Returns a random color.
-  static Color next() {
-    return new Color(0xFF000000 + _random.nextInt(0x00FFFFFF));
-  }
 }

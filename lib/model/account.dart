a1b9b4954c8a9a5291fe4867f310c886553685d8
Abhith Rajan <dearabhith@gmail.com>
2018-07-31 17:39:26 +0530

    currencyCode included

--- lib/model/account.dart
+++ lib/model/account.dart
@@ -1,14 +1,15 @@
 class Account {
-  final String id, name, accountType, currencySymbol;
+  final String id, name, accountType, currencySymbol, currencyCode;
   final double currentBalance;
 
   Account(this.id, this.name, this.accountType, this.currencySymbol,
-      this.currentBalance);
+      this.currentBalance, this.currencyCode);
 
   Account.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         name = json['name'],
         accountType = json['accountType'],
         currencySymbol = json['currency']['symbol'],
+        currencyCode = json['currency']['code'],
         currentBalance = json['currentBalance'];
 }

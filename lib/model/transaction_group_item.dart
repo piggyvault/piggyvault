498815b458cd9515941818a2ae7f68261813b00b
Abhith Rajan <dearabhith@gmail.com>
2018-08-07 22:43:33 +0530

    redundant index field removed  and groupBy enum added

--- lib/model/transaction_group_item.dart
+++ lib/model/transaction_group_item.dart
@@ -1,10 +1,11 @@
 import 'package:piggy_flutter/model/transaction.dart';
 
 class TransactionGroupItem {
-  final int index;
   final String title;
   List<Transaction> transactions = [];
   double totalInflow = 0.0, totalOutflow = 0.0;
 
-  TransactionGroupItem(this.index, this.title);
+  TransactionGroupItem({this.title});
 }
+
+enum TransactionsGroupBy { Date, Category }

eedc3a847698b98adc01b511f1b463d2265cdf48
Abhith Rajan <dearabhith@gmail.com>
2018-08-11 12:47:12 +0530

    common EmptyResultWidget

---
+++ lib/ui/widgets/common/empty_result_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:piggy_flutter/ui/widgets/common/message_placeholder.dart';
+
+class EmptyResultWidget extends StatelessWidget {
+  final bool visible;
+
+  const EmptyResultWidget({Key key, this.visible}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 300),
+      opacity: visible ? 1.0 : 0.0,
+      child: MessagePlaceholder(),
+    );
+  }
+}

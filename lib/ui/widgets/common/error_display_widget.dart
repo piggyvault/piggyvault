3fdbe7a41f4e4b8d137fcdb5f9749ce381ebbf00
Abhith Rajan <dearabhith@gmail.com>
2018-08-11 12:46:54 +0530

    common ErrorDisplayWidget

---
+++ lib/ui/widgets/common/error_display_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:piggy_flutter/ui/widgets/common/message_placeholder.dart';
+
+class ErrorDisplayWidget extends StatelessWidget {
+  final bool visible;
+
+  const ErrorDisplayWidget({Key key, this.visible}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 300),
+      opacity: visible ? 1.0 : 0.0,
+      child: MessagePlaceholder(
+        title: 'Something went wrong',
+        message: 'An error occured during the operation, please try again.',
+        icon: Icons.error_outline,
+      ),
+    );
+  }
+}

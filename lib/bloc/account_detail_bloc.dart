0ba4a4ab8454408333b9e7644cacf4c2b3e94f49
Abhith Rajan <dearabhith@gmail.com>
2018-08-13 14:30:11 +0530

    account detail handling added as well as bloc improvements

--- lib/bloc/account_detail_bloc.dart
+++ lib/bloc/account_detail_bloc.dart
@@ -1,44 +1,69 @@
 import 'dart:async';
 
 import 'package:intl/intl.dart';
+import 'package:piggy_flutter/model/account.dart';
 import 'package:piggy_flutter/model/account_detail_state.dart';
 import 'package:piggy_flutter/model/transaction_group_item.dart';
+import 'package:piggy_flutter/services/account_service.dart';
 import 'package:piggy_flutter/services/transaction_service.dart';
 import 'package:rxdart/rxdart.dart';
 
 class AccountDetailBloc {
   final String accountId;
-  int pageIndex = 0;
+  int _pageIndex = 0;
+  String _title = 'This Month',
+      _nextPageTitle = 'Future',
+      _previousPageTitle = 'Last Month';
 
   final TransactionService _transactionService = TransactionService();
+  final AccountService _accountService = AccountService();
 
+  final _account = BehaviorSubject<Account>();
   final _onPageChanged = PublishSubject<int>();
-  final _state = BehaviorSubject<AccountDetailState>(
-      seedValue: AccountDetailLoading(
-          title: 'This Month',
-          nextPageTitle: 'Future',
-          previousPageTitle: 'Last Month'));
+  final _transactionsState = BehaviorSubject<AccountDetailState>();
+  final _refreshAccount = PublishSubject<bool>();
 
   Function(int) get onPageChanged => _onPageChanged.sink.add;
+  Function(bool) get refreshAccount => _refreshAccount.sink.add;
+  Function(Account) get changeAccount => _account.sink.add;
 
-  Stream<AccountDetailState> get state => _state.stream;
+  Stream<Account> get account => _account.stream;
+  Stream<AccountDetailState> get transactionsState => _transactionsState.stream;
+  Stream<AccountDetailState> get state =>
+      Observable.combineLatest2(account, transactionsState,
+          (Account accountData, AccountDetailState transactionsStateData) {
+        transactionsStateData.account = accountData;
+        transactionsStateData.title = _title;
+        transactionsStateData.nextPageTitle = _nextPageTitle;
+        transactionsStateData.previousPageTitle = _previousPageTitle;
+        return transactionsStateData;
+      }).asBroadcastStream();
 
   AccountDetailBloc({this.accountId}) {
     _onPageChanged.listen(_getTransactions);
+    _refreshAccount.listen(_getAccountDetails);
   }
 
   void dispose() {
     _onPageChanged.close();
-    _state.close();
+    _transactionsState.close();
+    _refreshAccount.close();
+    _account.close();
+  }
+
+  Future<Null> _getAccountDetails(bool done) async {
+    print('######### AccountDetailBloc _getAccountDetails');
+    final result = await _accountService.getAccountDetails(accountId);
+    _account.add(result);
   }
 
   Future<Null> _getTransactions(
     int delta,
   ) async {
-    pageIndex += delta;
+    _pageIndex += delta;
 
-    print('######### AccountDetailBloc _getTransactions $pageIndex');
-    var startMonth = DateTime.now().month + pageIndex;
+    print('######### AccountDetailBloc _getTransactions $_pageIndex');
+    var startMonth = DateTime.now().month + _pageIndex;
     var startYear = DateTime.now().year;
 
     if (startMonth < 0) {
@@ -59,15 +84,12 @@ class AccountDetailBloc {
 
     final DateFormat formatter = DateFormat("MMM, ''yy");
 
-    final title = formatter.format(startDate);
-    final nextPageTitle = formatter.format(DateTime(endYear, endMonth, 1));
-    final previousPageTitle =
+    _title = formatter.format(startDate);
+    _nextPageTitle = formatter.format(DateTime(endYear, endMonth, 1));
+    _previousPageTitle =
         formatter.format(DateTime(startYear, startMonth - 1, 1));
 
-    _state.add(AccountDetailLoading(
-        title: title,
-        nextPageTitle: nextPageTitle,
-        previousPageTitle: previousPageTitle));
+    _transactionsState.add(AccountDetailLoading());
 
     final input = GetTransactionsInput(
         type: 'account',
@@ -80,22 +102,12 @@ class AccountDetailBloc {
       final result = await _transactionService.getTransactions(input);
 
       if (result.isEmpty) {
-        _state.add(AccountDetailEmpty(
-            title: title,
-            nextPageTitle: nextPageTitle,
-            previousPageTitle: previousPageTitle));
+        _transactionsState.add(AccountDetailEmpty());
       } else {
-        _state.add(AccountDetailPopulated(
-            result: result,
-            title: title,
-            nextPageTitle: nextPageTitle,
-            previousPageTitle: previousPageTitle));
+        _transactionsState.add(AccountDetailPopulated(result));
       }
     } catch (e) {
-      _state.add(AccountDetailError(
-          title: title,
-          nextPageTitle: nextPageTitle,
-          previousPageTitle: previousPageTitle));
+      _transactionsState.add(AccountDetailError());
     }
   }
 }

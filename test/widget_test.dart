7eb78296541d70b2de826b54ab399cc08c7346f2
Abhith Rajan <dearabhith@gmail.com>
2018-08-13 22:31:13 +0530

    redundant test commented

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,10 +4,10 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
-import 'package:piggy_flutter/main.dart';
+// import 'package:piggy_flutter/main.dart';
 
 void main() {
 //  testWidgets('Counter increments smoke test', (WidgetTester tester) async {

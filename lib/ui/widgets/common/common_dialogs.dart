2447a7d1053b4f504ac76804c3cd1885ece9a7a3
Abhith Rajan <dearabhith@gmail.com>
2018-08-15 23:15:57 +0530

    common dialogs from UI kit

---
+++ lib/ui/widgets/common/common_dialogs.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:piggy_flutter/services/network_service_response.dart';
+import 'package:piggy_flutter/utils/uidata.dart';
+
+fetchApiResult(BuildContext context, NetworkServiceResponse snapshot) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+          title: Text(UIData.error),
+          content: Text(snapshot.message),
+          actions: <Widget>[
+            FlatButton(
+              child: Text(UIData.ok),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ),
+  );
+}
+
+showSuccess(BuildContext context, String message, IconData icon) {
+  showDialog(
+      context: context,
+      builder: (context) => Center(
+            child: Material(
+              borderRadius: BorderRadius.circular(8.0),
+              color: Colors.black,
+              elevation: 5.0,
+              child: Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      icon,
+                      color: Colors.green,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      message,
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ));
+}
+
+showProgress(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.yellow,
+            ),
+          ));
+}
+
+hideProgress(BuildContext context) {
+  Navigator.pop(context);
+}

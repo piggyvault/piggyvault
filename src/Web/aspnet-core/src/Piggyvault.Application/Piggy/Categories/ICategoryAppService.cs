c016d2f9fc7a7e4fe62c92f638181e44cc158288
Abhith <dearabhith@gmail.com>
2019-10-18 23:25:54 +0400

    chore: import category app service from old project
    copied with minimal change to the 2 year old code

---
+++ src/Web/aspnet-core/src/Piggyvault.Application/Piggy/Categories/ICategoryAppService.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Piggyvault.Piggy.Categories.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piggyvault.Piggy.Categories
+{
+    public interface ICategoryAppService : IApplicationService
+    {
+        Task CreateOrUpdateCategory(CategoryEditDto input);
+
+        Task<CategoryEditDto> GetCategoryForEdit(EntityDto<Guid> input);
+
+        Task<ListResultDto<CategoryEditDto>> GetTenantCategories();
+    }
+}

0a7ce436d6699d066cb19994acf3dd2bcc231420
Abhith <dearabhith@gmail.com>
2019-10-18 19:57:11 +0400

    chore: add piggy tables

---
+++ src/Web/aspnet-core/src/Piggyvault.Core/Piggy/Accounts/AccountType.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Piggyvault.Piggy.Accounts
+{
+    [Table("PvAccountType")]
+    public class AccountType : Entity
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public virtual string Name { get; set; }
+    }
+}
---
+++ src/Web/aspnet-core/src/Piggyvault.Core/Piggy/Transactions/TransactionComment.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Piggyvault.Authorization.Users;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Piggyvault.Piggy.Transactions
+{
+    [Table("PvTransactionComment")]
+    public class TransactionComment : FullAuditedEntity<Guid, User>
+    {
+        public string Content { get; set; }
+
+        [ForeignKey("TransactionId")]
+        public virtual Transaction Transaction { get; set; }
+
+        public Guid TransactionId { get; set; }
+    }
+}

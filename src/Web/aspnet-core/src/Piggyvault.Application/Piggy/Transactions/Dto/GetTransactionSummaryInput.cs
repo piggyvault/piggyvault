7c69ee8a18934459967a99c8fbf60d60945d0a6d
Abhith <dearabhith@gmail.com>
2019-11-14 21:47:45 +0400

    feat: add transaction summary

---
+++ src/Web/aspnet-core/src/Piggyvault.Application/Piggy/Transactions/Dto/GetTransactionSummaryInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piggyvault.Piggy.Transactions.Dto
+{
+    public class GetTransactionSummaryInput
+    {
+        public Guid? AccountId { get; set; }
+        public string Duration { get; set; }
+        public DateTime? EndDate { get; set; }
+        public DateTime? StartDate { get; set; }
+    }
+}

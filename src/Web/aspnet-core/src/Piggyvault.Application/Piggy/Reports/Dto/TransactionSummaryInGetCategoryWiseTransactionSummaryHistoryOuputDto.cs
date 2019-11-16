3f4c6dfa33d82b4f64fcadb3ab4508f7e8d67ee1
Abhith <dearabhith@gmail.com>
2019-11-16 18:14:30 +0400

    feat: add category wise report

---
+++ src/Web/aspnet-core/src/Piggyvault.Application/Piggy/Reports/Dto/TransactionSummaryInGetCategoryWiseTransactionSummaryHistoryOuputDto.cs
@@ -0,0 +1,12 @@
+﻿using Piggyvault.Piggy.Transactions.Dto;
+using System.Collections.Generic;
+
+namespace Piggyvault.Piggy.Reports.Dto
+{
+    public class TransactionSummaryInGetCategoryWiseTransactionSummaryHistoryOuputDto
+    {
+        public decimal Total { get; set; }
+
+        public virtual List<TransactionPreviewDto> Transactions { get; set; }
+    }
+}

7f612852f0bc555ac5f357b947d1b9d33efc11e5
Abhith <dearabhith@gmail.com>
2019-11-18 22:11:32 +0400

    feat: enable push notifications

---
+++ src/Web/aspnet-core/src/Piggyvault.Application/PiggySettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piggyvault
+{
+    public class OneSignalSettings
+    {
+        public string ApiKey { get; set; }
+        public string AppId { get; set; }
+    }
+
+    public class PiggySettings
+    {
+        public OneSignalSettings OneSignal { get; set; }
+    }
+}

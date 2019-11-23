9bcfd8ff5c78c2230a2deeee43921258e72e6994
Abhith <dearabhith@gmail.com>
2019-11-23 11:41:55 +0400

    refactor: accounts menu on sidenav

--- src/Mobile/piggy_flutter/lib/widgets/common/common_drawer.dart
+++ src/Mobile/piggy_flutter/lib/widgets/common/common_drawer.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:piggy_flutter/blocs/account_bloc.dart';
+import 'package:piggy_flutter/blocs/accounts/accounts_bloc.dart';
+import 'package:piggy_flutter/blocs/accounts/accounts_state.dart';
 import 'package:piggy_flutter/blocs/auth/auth.dart';
 import 'package:piggy_flutter/blocs/bloc_provider.dart' as oldBloc;
 import 'package:piggy_flutter/blocs/category_bloc.dart';
-import 'package:piggy_flutter/models/account.dart';
 import 'package:piggy_flutter/models/category.dart';
 import 'package:piggy_flutter/screens/reports/categorywise_recent_months_report_screen.dart';
 import 'package:piggy_flutter/user/user.dart';
@@ -20,8 +20,6 @@ class CommonDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     final CategoryBloc categoryBloc =
         oldBloc.BlocProvider.of<CategoryBloc>(context);
-    final AccountBloc accountBloc =
-        oldBloc.BlocProvider.of<AccountBloc>(context);
 
     return Drawer(
       child: ListView(
@@ -40,7 +38,34 @@ class CommonDrawer extends StatelessWidget {
             onTap: (() => Navigator.of(context)
                 .pushReplacementNamed(UIData.dashboardRoute)),
           ),
-          accountsTile(accountBloc),
+          ListTile(
+              title: Text(
+                "Accounts",
+                style: menuTextStyle,
+              ),
+              leading: Icon(
+                Icons.account_balance_wallet,
+                color: Colors.green,
+              ),
+              onTap: (() => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => HomePage(
+                              startpage: StartPage.Accounts,
+                            )),
+                  )),
+              trailing: BlocBuilder<AccountsBloc, AccountsState>(
+                  builder: (context, state) {
+                if (state is AccountsLoaded) {
+                  return Chip(
+                    backgroundColor: Colors.green,
+                    label: Text(state.userAccounts.length.toString()),
+                  );
+                }
+                return Chip(
+                  label: Icon(Icons.hourglass_empty),
+                );
+              })),
           categoriesTile(categoryBloc),
           ListTile(
             title: Text(
@@ -93,40 +118,6 @@ class CommonDrawer extends StatelessWidget {
               ? Chip(
                   key: ValueKey<String>(snapshot.data.length.toString()),
                   backgroundColor: Colors.cyan,
-                  label: Text(snapshot.data.length.toString()),
-                )
-              : Chip(
-                  label: Icon(Icons.hourglass_empty),
-                ),
-        );
-      },
-    );
-  }
-
-  Widget accountsTile(AccountBloc accountBloc) {
-    return StreamBuilder<List<Account>>(
-      stream: accountBloc.userAccounts,
-      builder: (context, snapshot) {
-        return ListTile(
-          title: Text(
-            "Accounts",
-            style: menuTextStyle,
-          ),
-          leading: Icon(
-            Icons.account_balance_wallet,
-            color: Colors.green,
-          ),
-          onTap: (() => Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => HomePage(
-                          startpage: StartPage.Accounts,
-                        )),
-              )),
-          trailing: snapshot.hasData
-              ? Chip(
-                  key: ValueKey<String>(snapshot.data.length.toString()),
-                  backgroundColor: Colors.green,
                   label: Text(snapshot.data.length.toString()),
                 )
               : Chip(

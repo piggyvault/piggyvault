60bf51510ca399208feeb3c616222f64e777b79b
Abhith <dearabhith@gmail.com>
2019-11-26 22:32:38 +0400

    feat: add number of transactions on account details

--- src/Mobile/piggy_flutter/lib/screens/account/account_detail.dart
+++ src/Mobile/piggy_flutter/lib/screens/account/account_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:piggy_flutter/blocs/account/bloc.dart';
 import 'package:piggy_flutter/blocs/account_transactions/bloc.dart';
@@ -104,91 +105,69 @@ class _AccountDetailPageState extends State<AccountDetailPage>
                                   (BuildContext context, int index) {
                                 return Column(
                                   children: <Widget>[
-                                    // getSearchBarUI(),
+                                    getSearchBarUI(),
                                     getTimeDateUI(
                                         accountTransactionsBloc, accountBloc),
                                   ],
                                 );
                               }, childCount: 1),
                             ),
-                            // SliverPersistentHeader(
-                            //   pinned: true,
-                            //   floating: true,
-                            //   delegate: ContestTabHeader(
-                            //     getFilterBarUI(),
-                            //   ),
-                            // ),
+                            SliverPersistentHeader(
+                              pinned: true,
+                              floating: true,
+                              delegate: ContestTabHeader(
+                                getFilterBarUI(),
+                              ),
+                            ),
                           ];
                         },
                         body: Container(
                           color:
                               PiggyAppTheme.buildLightTheme().backgroundColor,
                           child: BlocBuilder<AccountTransactionsBloc,
-                                  AccountTransactionsState>(
-                              bloc: accountTransactionsBloc,
-                              builder: (context, state) {
-                                return SafeArea(
-                                  top: false,
-                                  bottom: false,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Stack(
-                                          children: <Widget>[
-                                            // Fade in a loading screen when results are being fetched
-                                            LoadingWidget(
-                                                visible: state
-                                                    is AccountTransactionsLoading),
+                              AccountTransactionsState>(
+                            bloc: accountTransactionsBloc,
+                            builder: (context, state) {
+                              return SafeArea(
+                                top: false,
+                                bottom: false,
+                                child: Column(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Stack(
+                                        children: <Widget>[
+                                          // Fade in a loading screen when results are being fetched
+                                          LoadingWidget(
+                                              visible: state
+                                                  is AccountTransactionsLoading),
 
-                                            // Fade in an Empty Result screen if the search contained
-                                            // no items
-                                            EmptyResultWidget(
-                                                visible: state
-                                                    is AccountTransactionsEmpty),
+                                          // Fade in an Empty Result screen if the search contained
+                                          // no items
+                                          EmptyResultWidget(
+                                              visible: state
+                                                  is AccountTransactionsEmpty),
 
-                                            // Fade in an error if something went wrong when fetching
-                                            // the results
-                                            ErrorDisplayWidget(
-                                                visible: state
-                                                    is AccountTransactionsError),
+                                          // Fade in an error if something went wrong when fetching
+                                          // the results
+                                          ErrorDisplayWidget(
+                                              visible: state
+                                                  is AccountTransactionsError),
 
-                                            // Fade in the Result if available
-                                            TransactionList(
-                                              items: state
-                                                      is AccountTransactionsLoaded
-                                                  ? state.result.sections
-                                                  : [],
-                                            ),
-                                          ],
-                                        ),
+                                          // Fade in the Result if available
+                                          TransactionList(
+                                            items: state
+                                                    is AccountTransactionsLoaded
+                                                ? state.result.sections
+                                                : [],
+                                          ),
+                                        ],
                                       ),
-                                    ],
-                                  ),
-                                );
-                              }),
-                          // child: ListView.builder(
-                          //   itemCount: hotelList.length,
-                          //   padding: const EdgeInsets.only(top: 8),
-                          //   scrollDirection: Axis.vertical,
-                          //   itemBuilder: (BuildContext context, int index) {
-                          //     final int count =
-                          //         hotelList.length > 10 ? 10 : hotelList.length;
-                          //     final Animation<double> animation =
-                          //         Tween<double>(begin: 0.0, end: 1.0).animate(
-                          //             CurvedAnimation(
-                          //                 parent: animationController,
-                          //                 curve: Interval(
-                          //                     (1 / count) * index, 1.0,
-                          //                     curve: Curves.fastOutSlowIn)));
-                          //     animationController.forward();
-                          //     return HotelListView(
-                          //       callback: () {},
-                          //       hotelData: hotelList[index],
-                          //       animation: animation,
-                          //       animationController: animationController,
-                          //     );
-                          //   },
-                          // ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
                         ),
                       ),
                     )
@@ -198,6 +177,81 @@ class _AccountDetailPageState extends State<AccountDetailPage>
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget getSearchBarUI() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16, top: 8, bottom: 8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: PiggyAppTheme.buildLightTheme().backgroundColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(38.0),
+                  ),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        offset: const Offset(0, 2),
+                        blurRadius: 8.0),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 16, right: 16, top: 4, bottom: 4),
+                  child: TextField(
+                    onChanged: (String txt) {},
+                    style: const TextStyle(
+                      fontSize: 18,
+                    ),
+                    cursorColor: PiggyAppTheme.buildLightTheme().primaryColor,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Coming soon...',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: PiggyAppTheme.buildLightTheme().primaryColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(38.0),
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.4),
+                    offset: const Offset(0, 2),
+                    blurRadius: 8.0),
+              ],
+            ),
+            child: Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(32.0),
+                ),
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Icon(FontAwesomeIcons.search,
+                      size: 20,
+                      color: PiggyAppTheme.buildLightTheme().backgroundColor),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -358,6 +412,142 @@ class _AccountDetailPageState extends State<AccountDetailPage>
     );
   }
 
+  Widget getFilterBarUI() {
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 24,
+            decoration: BoxDecoration(
+              color: PiggyAppTheme.buildLightTheme().backgroundColor,
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    offset: const Offset(0, -2),
+                    blurRadius: 8.0),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          color: PiggyAppTheme.buildLightTheme().backgroundColor,
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 4),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: BlocBuilder<AccountTransactionsBloc,
+                          AccountTransactionsState>(
+                      bloc: accountTransactionsBloc,
+                      builder: (context, state) {
+                        if (state is AccountTransactionsLoaded) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '${state.result.transactions.length} transactions found',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                fontSize: 16,
+                              ),
+                            ),
+                          );
+                        }
+                        if (state is AccountTransactionsEmpty) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              '0 transactions found',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                fontSize: 16,
+                              ),
+                            ),
+                          );
+                        }
+
+                        if (state is AccountTransactionsLoading) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SpinKitThreeBounce(
+                              color:
+                                  PiggyAppTheme.buildLightTheme().primaryColor,
+                              size: 16,
+                            ),
+                          );
+                        }
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            '---',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                              fontSize: 16,
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    focusColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    splashColor: Colors.grey.withOpacity(0.2),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(4.0),
+                    ),
+                    // onTap: () {
+                    //   FocusScope.of(context).requestFocus(FocusNode());
+                    //   Navigator.push<dynamic>(
+                    //     context,
+                    //     MaterialPageRoute<dynamic>(
+                    //         builder: (BuildContext context) => FiltersScreen(),
+                    //         fullscreenDialog: true),
+                    //   );
+                    // },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Filtter',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w100,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(Icons.sort,
+                                color: PiggyAppTheme.buildLightTheme()
+                                    .primaryColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: Divider(
+            height: 1,
+          ),
+        )
+      ],
+    );
+  }
+
   Widget getAppBarUI() {
     return AppBar(
       title: Text(widget.account.name),
@@ -430,5 +620,29 @@ class _AccountDetailPageState extends State<AccountDetailPage>
         onCancelClick: () {},
       ),
     );
+  }
+}
+
+class ContestTabHeader extends SliverPersistentHeaderDelegate {
+  ContestTabHeader(
+    this.searchUI,
+  );
+  final Widget searchUI;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return searchUI;
+  }
+
+  @override
+  double get maxExtent => 52.0;
+
+  @override
+  double get minExtent => 52.0;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }

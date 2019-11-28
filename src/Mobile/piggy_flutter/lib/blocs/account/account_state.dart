f6d929a69603f182bc5463d15371d2ec12df07f0
Abhith <dearabhith@gmail.com>
2019-11-28 22:33:07 +0400

    feat: refresh account balance on new transaction

--- src/Mobile/piggy_flutter/lib/blocs/account/account_state.dart
+++ src/Mobile/piggy_flutter/lib/blocs/account/account_state.dart
@@ -3,19 +3,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:piggy_flutter/models/models.dart';
 
 abstract class AccountState extends Equatable {
-  const AccountState();
+  final String accountId;
+
+  const AccountState(this.accountId);
+
   @override
   List<Object> get props => [];
 }
 
-class AccountEmpty extends AccountState {}
+class AccountEmpty extends AccountState {
+  AccountEmpty(String accountId) : super(accountId);
+}
 
-class AccountLoading extends AccountState {}
+class AccountLoading extends AccountState {
+  AccountLoading(String accountId) : super(accountId);
+}
 
 class AccountLoaded extends AccountState {
   final Account account;
 
-  AccountLoaded({@required this.account}) : assert(account != null);
+  AccountLoaded({@required this.account}) : super(account.id);
 }
 
-class AccountFetchError extends AccountState {}
+class AccountFetchError extends AccountState {
+  AccountFetchError(String accountId) : super(accountId);
+}

16d86022436555d6694563e7cc0353a98345799d
Abhith <dearabhith@gmail.com>
2019-12-15 20:56:17 +0400

    refactor: flutter analysis recommendations

--- src/Mobile/piggy_flutter/lib/blocs/auth/auth_bloc.dart
+++ src/Mobile/piggy_flutter/lib/blocs/auth/auth_bloc.dart
@@ -3,13 +3,14 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:piggy_flutter/blocs/auth/auth.dart';
+import 'package:piggy_flutter/models/user.dart';
 
 import 'package:piggy_flutter/repositories/repositories.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  final UserRepository userRepository;
-
   AuthBloc({@required this.userRepository}) : assert(userRepository != null);
+
+  final UserRepository userRepository;
 
   @override
   AuthState get initialState => AuthUninitialized();
@@ -26,7 +27,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       } else {
         final bool hasToken = await userRepository.hasToken();
         if (hasToken) {
-          final user = await userRepository.getCurrentLoginInformation();
+          final User user = await userRepository.getCurrentLoginInformation();
           if (user == null || user.id == null) {
             yield AuthUnauthenticated();
           } else {
@@ -41,8 +42,15 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     if (event is LoggedIn) {
       yield AuthLoading();
       await userRepository.persistToken(event.token);
-      _handleSendTags(event.tenancyName);
-      final user = await userRepository.getCurrentLoginInformation();
+
+      try {
+        await OneSignal.shared
+            .sendTag('tenancyName', event.tenancyName.trim().toLowerCase());
+      } catch (e) {
+        print(e);
+      }
+
+      final User user = await userRepository.getCurrentLoginInformation();
       if (user == null || user.id == null) {
         yield AuthUnauthenticated();
       } else {
@@ -54,46 +62,29 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     if (event is LoggedOut) {
       yield AuthLoading();
 
-      _handleDeleteTag();
+      try {
+        await OneSignal.shared.deleteTag('tenancyName');
+      } catch (error) {
+        print(error);
+      }
       await userRepository.deleteToken();
       yield AuthUnauthenticated();
     }
   }
 
-  void _handleSendTags(String tenancyName) {
-    try {
-      // print("Sending tags");
-      OneSignal.shared
-          .sendTag("tenancyName", tenancyName.trim().toLowerCase())
-          .then((response) {
-        // print("Successfully sent tags with response: $response");
-      }).catchError((error) {
-        // print("Encountered an error sending tags: $error");
-      });
-    } catch (e) {}
-  }
-
   // Platform messages are asynchronous, so we initialize in an async method.
   void initOnesignal() {
-    OneSignal.shared.init("9bf198c9-442b-4619-b5c9-759fc250f15b", iOSSettings: {
+    OneSignal.shared.init('9bf198c9-442b-4619-b5c9-759fc250f15b', iOSSettings: {
       OSiOSSettings.autoPrompt: false,
       OSiOSSettings.inAppLaunchUrl: true
     });
     OneSignal.shared
         .setInFocusDisplayType(OSNotificationDisplayType.notification);
     // OneSignal.shared.setLogLevel(OSLogLevel.warn, OSLogLevel.none);
-    OneSignal.shared.setNotificationReceivedHandler((notification) {
+    OneSignal.shared
+        .setNotificationReceivedHandler((OSNotification notification) {
       // print(
       //     "Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}");
-    });
-  }
-
-  void _handleDeleteTag() {
-    // print("Deleting tag");
-    OneSignal.shared.deleteTag("tenancyName").then((response) {
-      // print("Successfully deleted tags with response $response");
-    }).catchError((error) {
-      // print("Encountered error deleting tag: $error");
     });
   }
 }

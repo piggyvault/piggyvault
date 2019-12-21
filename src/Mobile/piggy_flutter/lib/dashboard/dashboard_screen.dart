5decfe057e1864ff1315cf8526a987f5766385a2
Abhith <dearabhith@gmail.com>
2019-12-21 15:14:41 +0400

    refactor: remove unused sections from old dashboard

--- src/Mobile/piggy_flutter/lib/dashboard/dashboard_screen.dart
+++ src/Mobile/piggy_flutter/lib/dashboard/dashboard_screen.dart
@@ -292,34 +292,6 @@ class DashboardScreenState extends State<DashboardScreen> {
           mainAxisSpacing: 12.0,
           padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
           children: <Widget>[
-            _balanceTile(),
-            _transactionsCountTile(),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Material(
-                          color: Colors.amber,
-                          shape: CircleBorder(),
-                          child: Padding(
-                            padding: EdgeInsets.all(16.0),
-                            child: Icon(Icons.multiline_chart,
-                                color: Colors.white, size: 30.0),
-                          )),
-                      Padding(padding: EdgeInsets.only(bottom: 16.0)),
-                      Text('Reports',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24.0)),
-                      Text('Categorywise Recent',
-                          style: TextStyle(color: Colors.black45)),
-                    ]),
-              ),
-            ),
             _buildTile(
               Padding(
                   padding: const EdgeInsets.all(24.0),
@@ -373,14 +345,9 @@ class DashboardScreenState extends State<DashboardScreen> {
                     ],
                   )),
             ),
-            _lastTransactionTile()
           ],
           staggeredTiles: [
-            StaggeredTile.extent(2, 110.0),
-            StaggeredTile.extent(1, 180.0),
-            StaggeredTile.extent(1, 180.0),
             StaggeredTile.extent(2, 220.0),
-            StaggeredTile.extent(2, 110.0),
           ],
         );
       },
@@ -390,138 +357,6 @@ class DashboardScreenState extends State<DashboardScreen> {
   void _load([bool isError = false]) {
     widget._dashboardBloc.add(UnDashboardEvent());
     widget._dashboardBloc.add(LoadDashboardEvent(isError));
-  }
-
-  Widget _transactionsCountTile() {
-    return _buildTile(Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Material(
-                color: Colors.teal,
-                shape: CircleBorder(),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Icon(Icons.local_atm, color: Colors.white, size: 30.0),
-                )),
-            Padding(padding: EdgeInsets.only(bottom: 16.0)),
-            BlocBuilder<TransactionSummaryBloc, TransactionSummaryState>(
-              builder: (context, state) {
-                if (state is TransactionSummaryLoaded) {
-                  return Text(
-                      '${state.summary.totalFamilyTransactionsCount.toString()}',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 24.0));
-                } else if (state is TransactionSummaryLoading) {
-                  return CircularProgressIndicator();
-                }
-
-                return Text('---');
-              },
-            ),
-            Text('Transactions', style: TextStyle(color: Colors.black45)),
-          ]),
-    ));
-  }
-
-  Widget _balanceTile() {
-    return _buildTile(
-      Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Balance', style: TextStyle(color: Colors.blueAccent)),
-                  BlocBuilder<TransactionSummaryBloc, TransactionSummaryState>(
-                    builder: (context, state) {
-                      if (state is TransactionSummaryLoaded) {
-                        return Text(
-                            '₹ ${state.summary.userNetWorth.toString()}',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 34.0));
-                      } else if (state is TransactionSummaryLoading) {
-                        return CircularProgressIndicator();
-                      }
-
-                      return Text('---');
-                    },
-                  )
-                ],
-              ),
-              Material(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(24.0),
-                  child: Center(
-                      child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child:
-                        Icon(Icons.timeline, color: Colors.white, size: 30.0),
-                  )))
-            ]),
-      ),
-    );
-  }
-
-  Widget _lastTransactionTile() {
-    return _buildTile(
-      Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Last Transaction On',
-                      style: TextStyle(color: Colors.redAccent)),
-                  BlocBuilder<RecentTransactionsBloc, RecentTransactionsState>(
-                      builder: (context, state) {
-                    if (state is RecentTransactionsLoaded) {
-                      return Text(state.latestTransactionDate,
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 30.0));
-                    }
-
-                    if (state is RecentTransactionsLoading) {
-                      return CircularProgressIndicator();
-                    }
-
-                    return Text('Data Not Available');
-                  })
-                ],
-              ),
-              Material(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(24.0),
-                  child: Center(
-                      child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Icon(Icons.add, color: Colors.white, size: 30.0),
-                  )))
-            ]),
-      ),
-      onTap: () =>
-          Navigator.of(context).push(MaterialPageRoute<DismissDialogAction>(
-              builder: (_) => TransactionFormPage(
-                    transactionsBloc: BlocProvider.of<TransactionBloc>(context),
-                  ),
-              fullscreenDialog: true)),
-    );
   }
 
   Widget _buildTile(Widget child, {Function() onTap}) {

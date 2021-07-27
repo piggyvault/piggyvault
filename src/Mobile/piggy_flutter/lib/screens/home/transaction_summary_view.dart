c3d43ec87ac903923fee95e782419163a48f0082
Abhith Rajan <dearabhith@gmail.com>
2021-07-27 22:06:01 +0400

    refactor(flutter): replace deprecated list initialization

--- src/Mobile/piggy_flutter/lib/screens/home/transaction_summary_view.dart
+++ src/Mobile/piggy_flutter/lib/screens/home/transaction_summary_view.dart
@@ -623,7 +623,7 @@ class CurvePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    List<Color> colorsList = List<Color>();
+    List<Color> colorsList = [];
     if (colors != null) {
       colorsList = colors;
     } else {

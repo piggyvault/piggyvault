15aa979ba952ce1316232c00b7ec5d1c0e0a834c
Abhith Rajan <dearabhith@gmail.com>
2021-08-04 19:59:43 +0400

    ui: remove search icon from search bar in recent

--- src/Mobile/piggy_flutter/lib/screens/home/recent_transactions.dart
+++ src/Mobile/piggy_flutter/lib/screens/home/recent_transactions.dart
@@ -261,7 +261,7 @@ class _RecentTransactionsPageState extends State<RecentTransactionsPage>
         children: <Widget>[
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.only(right: 16, top: 8, bottom: 8),
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
               child: Container(
                 decoration: BoxDecoration(
                   color: PiggyAppTheme.buildLightTheme().backgroundColor,
@@ -290,37 +290,6 @@ class _RecentTransactionsPageState extends State<RecentTransactionsPage>
                       hintText: 'Search...',
                     ),
                   ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: PiggyAppTheme.buildLightTheme().primaryColor,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(38.0),
-              ),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                    color: Colors.grey.withOpacity(0.4),
-                    offset: const Offset(0, 2),
-                    blurRadius: 8.0),
-              ],
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: InkWell(
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(32.0),
-                ),
-                onTap: () {
-                  FocusScope.of(context).requestFocus(FocusNode());
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Icon(FontAwesome5Solid.search_dollar,
-                      size: 20,
-                      color: PiggyAppTheme.buildLightTheme().backgroundColor),
                 ),
               ),
             ),

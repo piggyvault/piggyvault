57672c01f61dfc759d2628ad4334af9e489db163
Abhith <dearabhith@gmail.com>
2023-08-05 14:09:38 +0400

    fix: conflict between flutter SearchBar and custom one with same name

--- src/Mobile/piggy_flutter/lib/screens/home/recent_transactions.dart
+++ src/Mobile/piggy_flutter/lib/screens/home/recent_transactions.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide SearchBar;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:intl/intl.dart';
